6e3064edd11798d97c6e164d657d19de733dcc9e
Priscillo Manuel <priscillo.m@gmail.com>
2021-12-14 20:57:42 +0800

    made a footer component for the page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from 'react'
 import Navbar from './components/Navbar'
 import CardComponent from './components/Card'
 import Jumbo from './components/Jumbo'
+import Footer from './components/Footer'
 
 import { Container, Row, Col } from 'react-bootstrap'
 export default function App(){
@@ -17,7 +18,7 @@ export default function App(){
           <Col><CardComponent/></Col>
         </Row>
       </Container>
-
+      <Footer/>
     </>
 
   )
