2f5dae65ef3ab21d435807b2494338102feb07df
Priscillo Manuel <priscillo.m@gmail.com>
2021-12-16 20:49:24 +0800

    added an About Me section by changing Jumbotron

--- src/components/Jumbo.js
+++ src/components/Jumbo.js
@@ -5,9 +5,9 @@ import { Jumbotron, Button } from 'react-bootstrap'
 export default function Jumbo(){
 	return(
 		<Jumbotron>
-			<h1>Hello World!</h1>
-			<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur hic reprehenderit soluta architecto, vero accusamus nesciunt, enim nostrum labore explicabo porro, aliquam ipsa aperiam ipsam assumenda repudiandae blanditiis numquam quam?</p>
-			<Button variant = "primary">Learn More</Button>
+			<h1>Hello visitors of my portfolio!</h1>
+			<p>This is an About Me Page where I share here about my latest hobbies and things I want to talk about that are interesting</p>
+			<Button variant = "primary">Click this to learn more!</Button>
 		</Jumbotron>
 
 
