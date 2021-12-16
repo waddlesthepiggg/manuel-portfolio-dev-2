869de8fb68c5a44843eb4c443d66b25e51070c42
Priscillo Manuel <priscillo.m@gmail.com>
2021-12-16 21:12:25 +0800

    Changed Navbar name and details of About Me section

--- src/components/Jumbo.js
+++ src/components/Jumbo.js
@@ -6,8 +6,8 @@ export default function Jumbo(){
 	return(
 		<Jumbotron>
 			<h1>Hello visitors of my portfolio!</h1>
-			<p>This is an About Me Page where I share here about my latest hobbies and things I want to talk about that are interesting</p>
-			<Button variant = "primary">Click this to learn more!</Button>
+			<p>This is an About Me Page where I share about my latest hobbies and interesting things I want to talk about</p>
+			<Button variant = "primary">Click this to learn more about me!</Button>
 		</Jumbotron>
 
 
