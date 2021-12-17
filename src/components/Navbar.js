00caa72b8db9bed2ace163054d13b04f1ed2818d
waddlesthepiggg <74053408+waddlesthepiggg@users.noreply.github.com>
2021-12-17 17:28:05 +0800

    Update Navbar.js
    Changed Navbar.Brand

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ import { Navbar, Nav } from 'react-bootstrap'
 export default function NavbarComponent(){
 	return(
 		<Navbar bg = "light" expand = "lg">
-		<Navbar.Brand href = "#home">Edited Navbar Again</Navbar.Brand>
+		<Navbar.Brand href = "#home">Edited Navbar</Navbar.Brand>
 			<Nav className = "mr-auto">
 				<Nav.Link href = "#home">Home</Nav.Link>
 				<Nav.Link href = "#link">Link</Nav.Link>
